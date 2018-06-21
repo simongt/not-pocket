38b4eba7ba79324212e1462746c83bee20d78c05
yaakov <ykvdvd@gmail.com>
2018-06-21 12:01:47 -0400

    started added links to app.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./style.css";
+import Home from "../Home";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+    <Router>
+      <div className="App">
+
+      </div>
+    </Router>
+    )
   }
 }
 
