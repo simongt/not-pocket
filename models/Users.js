9ee3837fd19f876eb1925b9d3522997cb354b8b1
Darrell DeCosta <dar.deco+git@gmail.com>
2018-06-21 16:57:21 -0400

    fixed typos

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,31 @@
 const db = require('../database/connection');
 
-const User = {};
+const Users = {};
 
 Users.all = () => {
-  return db.('SELECT * FROM users')
+  return db.any('SELECT * FROM users')
 }
+
+Users.one = (id) => {
+  return db.one(`
+    SELECT * FROM users where id =  $<id>   
+  `, id)
+}
+
+
+//Tests
+
+// Users.all()
+// .then(dbResponse => {
+//   console.table(dbResponse)
+// })
+
+// Users.one({
+//   id: 1
+// })
+// .then(dbResponse => {
+//   console.table(dbResponse)
+// })
+
 
 module.exports = Users;
