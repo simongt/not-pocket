310b0b9ebba469940564301e10d92f05ab19874e
Darrell DeCosta <dar.deco+git@gmail.com>
2018-06-21 17:45:23 -0400

    built out (VERY) basic auth

--- server.js
+++ server.js
@@ -3,11 +3,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 //instantiate Helmet (basic form protection)
 const helmet = require('helmet');
 app.use(helmet());
 
 
+//Set up auth
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const isLoggedIn = (request, response, next) => {
+  if (!request.session.loggedIn) {
+    return response.send("Sorry, you must be signed in for access to this page");
+  } else {
+    next();
+  }
+};
 
 //Set up persistent session storage
 const session = require("express-session");
@@ -17,9 +33,9 @@ const Stash = require('./models/Stash');
 const Users = require('./models/Users');
 
 if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
-    conString = `postgres://localhost:5432/${databaseName}`;
+  conString = `postgres://localhost:5432/${databaseName}`;
 } else if (process.env.NODE_ENV === "production") {
-   conString = process.env.DATABASE_URL;
+  conString = process.env.DATABASE_URL;
 }
 
 app.use(session({
@@ -58,9 +74,53 @@ app.get('/stash.json', (request, response) => {
 });
 
 app.get('/users.json', (request, response) => {
-  User.all()
+  Users.all()
     .then(user => {
       response.json(user)
+    });
+});
+
+
+app.post('/login', (request, response) => {
+  const username = request.body.username;
+  const plainTextPassword = request.body.password;
+  Users.findByUsername(username)
+    .then(dbResp => {
+      return bcrypt
+        .compare(plainTextPassword, dbResp.password_digest)
+        .then(res => {
+          if (res === true) {
+            request.session.loggedIn = true;
+            request.session.user_id = dbResp.user_id;
+            request.session.username = dbResp.username;
+            return response.send("you have logged in");
+          } else {
+            response.send("login failed");
+          }
+        });
+    });
+});
+
+app.get('/test', isLoggedIn,(request,response) => {
+  response.send("if you are reading this, you are logged in")
+})
+
+
+app.post('/register', (request, response) => {
+  const plainTextPassword = request.body.password;
+  bcrypt.hash(plainTextPassword, saltRounds)
+    .then(hash => {
+      userData = {
+        username: request.body.username,
+        password_digest: hash,
+      };
+      return Users.create(userData);
+    })
+    .then(dbResp => {
+      request.session.loggedIn = true;
+      request.session.user_id = dbResp.user_id;
+      request.session.username = dbResp.username;
+      response.send("new user registered");
     });
 });
 
