9f30032f56ab8cbab09b4babf530c2fbcbc3871f
Sy <simon.tsegay@gmail.com>
2018-06-21 14:33:16 -0400

    added constructor

--- src/AddStash/index.js
+++ src/AddStash/index.js
@@ -3,18 +3,25 @@ import "./style.css";
 
 // AddStash is formatted as a toggled aside
 class AddStash extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      stash_url: "",
+      is_public: false,
+      user_id: 0, // ?
+      created: false,
+    }
+    this.onFormChange = this.onFormChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
   render() {
     return (
-      <div className="AddStash">
 
-      </div>
-    )
+    );
   }
 }
-
-
-
-
-
 
 export default AddStash;
