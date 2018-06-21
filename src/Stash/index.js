2f4fc42c0d1e1f045f44d90c298e00de6bc11dcf
Eryl <eryl.murphy@gmail.com>
2018-06-21 14:59:00 -0400

    made stash component

---
+++ src/Stash/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Stash extends Component {
+  render() {
+    return (
+      <div className="Stash">
+
+      </div>
+    )
+  }
+}
+
+
+export default Stash;
