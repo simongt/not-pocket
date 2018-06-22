5e4822c227de53c76831aed44856ddeedd665d5b
Eryl <eryl.murphy@gmail.com>
2018-06-22 11:07:12 -0400

    boilerplate for login

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,1 +1,17 @@
 //Form for authentication
+
+import React, { Component } from "react";
+import "./style.css";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="log-in">
+
+      </div>
+    )
+  }
+}
+
+
+export default Login;
