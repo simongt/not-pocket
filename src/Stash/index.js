d5786d312a20389d19b587c31e47e2dd961aeb6c
Sy <simon.tsegay@gmail.com>
2018-06-23 14:05:11 -0400

    implemented view for stash (from code along)

--- src/Stash/index.js
+++ src/Stash/index.js
@@ -5,11 +5,10 @@ class Stash extends Component {
   render() {
     return (
       <div className="Stash">
-
+        <h1>{this.props.stash.stash_url}</h1>
       </div>
     )
   }
 }
-
 
 export default Stash;
