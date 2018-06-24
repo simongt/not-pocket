c86a257c6f3ce2c9408288682c3e380b60e0e389
Eryl <eryl.murphy@gmail.com>
2018-06-24 18:23:31 -0400

    Header with Burger imported

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,33 +1,22 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-import { slide as Menu } from 'react-burger-menu';
+import Burger from "../Burger";
 import "./style.css";
 
 
 class Header extends Component {
-  showSettings (event) {
-    event.preventDefault();
-  }
     render() {
       return (
         <div className="Header">
-      <h1>NotPocket</h1>
-          <button className="hambuger  teal  xlarge" onClick="toggle_open()">☰</button>
-      <Menu>
-            <a id="home" className="menu-item" href="/">Home</a>
-              <button onClick="toggle_close()" className="bar-header">Close &times;</button>
-
-            </Menu>
-
-          <header className="teal">
+          <h1>NotPocket</h1>
             <nav>
               <p> {this.props.placeholder} </p>
             </nav>
-          </header>
+            <Burger />
       </div>
-  )}
-}
+  )
 
-
-export default Header;
+  }}
+  
+  
+  export default Header;
