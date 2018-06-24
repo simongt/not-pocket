b00a06aa51d8a515041e4f7160e6673dc00b22c9
yaakov <ykvdvd@gmail.com>
2018-06-24 12:29:04 -0400

    switched to plan css for the moment, and added react-router to the card

--- src/Stash/index.js
+++ src/Stash/index.js
@@ -1,19 +1,34 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./style.css";
 
 class Stash extends Component {
   render() {
     return (
       <body>
-      <div className="Stash">
-        <div className="container">
-          <div>{this.props.stash.stash_url}</div>
+        <div className="Stash">
+          <div className="card">
+            <img src="#" alt="img here" />
+            <div className="container">
+              <h4><b>Title</b></h4>
+              <div><Link to="/{this.props.stash.stash_url}" >Read Article </Link></div>
+
+
+              <p> tags here</p>
         </div>
+      </div>
       </div>
       </body>
     )
   }
 }
+
+
+
+
+
+
+
 
 export default Stash;
 
