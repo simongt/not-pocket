9533fb4526f5c62f818a88644f6102ac605f144c
Sy <simon.tsegay@gmail.com>
2018-06-24 23:17:41 -0400

    updated card view

--- src/Stash/index.js
+++ src/Stash/index.js
@@ -12,9 +12,9 @@ class Stash extends Component {
           <div className="card">
             <img src={this.props.stash.card_image_url} alt={this.props.stash.card_site_name} />
             <div className="container">
-              <h4><b>{this.props.stash.card_site_name}</b></h4>
+              <h4><a href={this.props.stash.stash_url}> {this.props.stash.card_site_name}</a></h4>
               <div>
-              <a href={this.props.stash.stash_url}> {this.props.stash.stash_url}</a>
+              
               </div>
               <p>{this.props.stash.card_description}</p>
               <p>tags here</p>
