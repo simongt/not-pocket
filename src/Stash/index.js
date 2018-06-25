638bc57c80a77026759e6ae2c764f07ce832292f
yaakov <ykvdvd@gmail.com>
2018-06-25 11:11:27 -0400

    in the process of merging

--- src/Stash/index.js
+++ src/Stash/index.js
@@ -6,20 +6,26 @@ class Stash extends Component {
   render() {
     console.log(this.props.stash);
     return <body>
-        <div className="Stash">
-        {/* <div className="container"> */}
-          <div className="card">
-           <img src={this.props.stash.card_image_url} alt={this.props.stash.card_site_name} />
-
-          <div><a href={this.props.stash.stash_url} target="_blank"> {this.props.stash.card_site_name}</a></div>
-              <div>{this.props.stash.card_description}</div>
-              <div>tags here</div>
-
+      <div className="Stash">
+        <div className="card">
+          <a href={this.props.stash.stash_url}>
+            <img src={this.props.stash.card_image_url} alt={this.props.stash.card_site_name} />
+          </a>
+          <div className="container">
+            <h4>
+              <a href={this.props.stash.stash_url}>
+                {this.props.stash.card_title}
+              </a>
+              &nbsp; <i>({this.props.stash.card_site_name})</i>
+            </h4>
+            <p>{this.props.stash.card_description}</p>
+            <p>tags here</p>
           </div>
-        {/* </div> */}
         </div>
-      </body>;
+      </div>
+    </body>;
   }
 }
 
 export default Stash;
+
