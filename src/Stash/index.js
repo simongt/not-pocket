b0a8432ac372a10d2b6b25c3046e4793d20fbbdb
yaakov <ykvdvd@gmail.com>
2018-06-25 11:34:00 -0400

    link of image also opens in new window

--- src/Stash/index.js
+++ src/Stash/index.js
@@ -4,27 +4,24 @@ import "./style.css";
 
 class Stash extends Component {
   render() {
-    console.log(this.props.stash);
-    return <body>
+    return (
       <div className="Stash">
         <div className="card">
-          <a href={this.props.stash.stash_url}>
-            <img src={this.props.stash.card_image_url} alt={this.props.stash.card_site_name} />
-          </a>
-          <div className="container">
-            <h4>
-              <a href={this.props.stash.stash_url}>
-                {this.props.stash.card_title}
-              </a>
-              &nbsp; <i>({this.props.stash.card_site_name})</i>
-            </h4>
-            <p>{this.props.stash.card_description}</p>
-            <p>tags here</p>
-          </div>
+            <a href={this.props.stash.stash_url} target="_blank">
+              <img src={this.props.stash.card_image_url} alt={this.props.stash.card_site_name} />
+            </a>
+
+          <h4>
+            <a href={this.props.stash.stash_url} target="_blank">
+              {this.props.stash.card_title}
+            </a>
+            &nbsp; <i>({this.props.stash.card_site_name})</i>
+          </h4>
+          <p>{this.props.stash.card_description}</p>
+          <p>tags here</p>
         </div>
       </div>
-    </body>;
-  }
+      )}
 }
 
 export default Stash;
