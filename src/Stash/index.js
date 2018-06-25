56b473ebf78f0d42a2b33bb732ec2796a8a5c39b
Sy <simon.tsegay@gmail.com>
2018-06-24 23:41:05 -0400

    fixed title, added link to image

--- src/Stash/index.js
+++ src/Stash/index.js
@@ -5,20 +5,25 @@ import "./style.css";
 class Stash extends Component {
   render() {
     console.log(this.props.stash);
-    return (
-      <body>
+    return <body>
         <div className="Stash">
           <div className="card">
-            <img src={this.props.stash.card_image_url} alt={this.props.stash.card_site_name} />
+            <a href={this.props.stash.stash_url}>
+              <img src={this.props.stash.card_image_url} alt={this.props.stash.card_site_name} />
+            </a>
             <div className="container">
-              <h4><a href={this.props.stash.stash_url}> {this.props.stash.card_site_name}</a></h4>
+              <h4>
+                <a href={this.props.stash.stash_url}>
+                  {this.props.stash.card_title}
+                </a>
+              &nbsp; <i>({this.props.stash.card_site_name})</i>
+              </h4>
               <p>{this.props.stash.card_description}</p>
               <p>tags here</p>
             </div>
           </div>
         </div>
-      </body>
-    )
+      </body>;
   }
 }
 
